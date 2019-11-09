843c5771b988e686245eef63bda8dd51ec084207
milanlazic <milanlazicgn@gmail.com>
2019-11-09 19:44:39 +0100

    update

--- randomize.js
+++ randomize.js
@@ -2,8 +2,7 @@ function randomize(arr) {
   const arrLen = arr.length;
   let randomArray = [];
   for(let i = 0; i < arrLen; i++) {
-    let tempArray = arr;
-    const rand = tempArray.splice(Math.floor(Math.random()*tempArray.length), 1);
+    const rand = arr.splice(Math.floor(Math.random()*arr.length), 1);
     randomArray.push(rand[0]);   
   }
   return randomArray;
